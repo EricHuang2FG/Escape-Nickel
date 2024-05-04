f0ae889931644c6d0a17380e78239425abac2980
EricHuang2FG <xilahuade@foxmail.com>
2024-05-04 14:42:48 -0400

    Added Obstacles

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import pygame, os
+import pygame, os, random
 
 pygame.init()
 WINDOW = pygame.display.set_mode((900, 500))
 pygame.display.set_caption("Escape Nickel")
-FPS = 30
+FPS = 60
 
 RED_GRAY = (46, 40, 40)
 ASSETSPATH = os.path.abspath(os.getcwd()) + "/Assets/"
 
 class Ground:
+    speed = 7
+
     def __init__(self, x):
         self.picture = pygame.image.load(ASSETSPATH + "ground.png")
         self.x = x
         self.y = WINDOW.get_height() - self.picture.get_height()
         self.width = self.picture.get_width()
         self.height = self.picture.get_height()
-        self.speed = 10
 
     def move(self):
         self.x -= self.speed
@@ -27,7 +28,33 @@ class Ground:
         self.move()
         self.draw()
 
+class Obstacle:
+    scale = 0.07
+
+    def __init__(self, x):
+        self.rawPicture = pygame.image.load(ASSETSPATH + "obstacle.png")
+        if random.randint(1, 2) == 1:
+            self.picture = pygame.transform.scale(self.rawPicture, (int(self.rawPicture.get_width() * self.scale), int(self.rawPicture.get_height() * (self.scale))))
+        else:
+            self.picture = pygame.transform.scale(self.rawPicture, (int(self.rawPicture.get_width() * self.scale), int(self.rawPicture.get_height() * (self.scale + 0.05))))
+        self.x = x
+        self.y = WINDOW.get_height() - 160 - self.picture.get_height()
+        self.width = self.picture.get_width()
+        self.height = self.picture.get_height()
+        self.speed = Ground.speed
+    
+    def move(self):
+        self.x -= self.speed
+
+    def draw(self):
+        WINDOW.blit(self.picture, (self.x, self.y))
+    
+    def behave(self):
+        self.move()
+        self.draw()
+
 ground = []
+obstacles = [Obstacle(WINDOW.get_width() + int(0.5 * WINDOW.get_width()))]
 
 def generateGround():
     position = 0
@@ -35,11 +62,26 @@ def generateGround():
         ground.append(Ground(position))
         position += WINDOW.get_width()
 
-def moveGround():
+def behaveGround():
     for segment in ground:
         segment.behave()
         if segment.x + segment.width < 0:
             segment.x += len(ground) * segment.width
+
+def spawnObstacles():
+    position = obstacles[-1].x
+    minSpacing = int(WINDOW.get_width() / 4)
+    maxSpacing = WINDOW.get_width()
+    while len(obstacles) < 4:
+        x = position + random.randint(minSpacing, maxSpacing)
+        obstacles.append(Obstacle(x))
+        position = x
+
+def behaveObstacles():
+    for obstacle in obstacles:
+        obstacle.behave()
+    if obstacles[0].x + obstacles[0].width < 0:
+        obstacles.pop(0)
 
 def checkRun():
     for event in pygame.event.get():
@@ -65,7 +107,9 @@ def main():
 
         if game == "game screen":
             drawGameBackground()
-            moveGround()
+            behaveGround()
+            spawnObstacles()
+            behaveObstacles()
 
         if game == "fail screen":
             pass
